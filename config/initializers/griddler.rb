e9cd46592358553b479fd95cfb44967b339a4dfb
duc <can.du@lixil.com>
2021-10-14 05:52:45 +0000

    fix griddler initializer with db:migrate

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -1,19 +1,26 @@
 require 'email_processor'
 
-Griddler.configure do |config|
-  config.processor_class = EmailProcessor # CommentViaEmail
-  config.processor_method = :process # :create_comment (A method on CommentViaEmail)
-  config.reply_delimiter = '--- Make sure your reply appears above this line ---'
-  config.email_service = AppSettings["email.mail_service"].present? ? AppSettings["email.mail_service"].to_sym : :sendgrid
+begin
+
+  Griddler.configure do |config|
+    config.processor_class = EmailProcessor # CommentViaEmail
+    config.processor_method = :process # :create_comment (A method on CommentViaEmail)
+    config.reply_delimiter = '--- Make sure your reply appears above this line ---'
+    config.email_service = AppSettings["email.mail_service"].present? ? AppSettings["email.mail_service"].to_sym : :sendgrid
+  end
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => AppSettings["email.mail_smtp"],
+    :port                 => AppSettings["email.mail_port"],
+    :user_name            => AppSettings["email.smtp_mail_username"].presence,
+    :password             => AppSettings["email.smtp_mail_password"].presence,
+    :domain               => AppSettings["email.mail_domain"],
+    :enable_starttls_auto => !AppSettings["email.mail_smtp"].in?(["localhost", "127.0.0.1", "::1"])
+  }
+
+  ActionMailer::Base.perform_deliveries = AppSettings['email.send_email'] == 'true'
+
+rescue => e
+  # run this module before database initialized may cause error (db:migrate)
+  puts e
 end
-
-ActionMailer::Base.smtp_settings = {
-  :address              => AppSettings["email.mail_smtp"],
-  :port                 => AppSettings["email.mail_port"],
-  :user_name            => AppSettings["email.smtp_mail_username"].presence,
-  :password             => AppSettings["email.smtp_mail_password"].presence,
-  :domain               => AppSettings["email.mail_domain"],
-  :enable_starttls_auto => !AppSettings["email.mail_smtp"].in?(["localhost", "127.0.0.1", "::1"])
-}
-
-ActionMailer::Base.perform_deliveries = AppSettings['email.send_email'] == 'true'
